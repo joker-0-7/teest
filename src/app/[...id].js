26372410f44d1f789a2b2b70e13ef6475d04a9fd
joker-0-7 <jokere136@gmail.com>
2023-08-27 16:44:42 +0300

    df

--- src/app/[...id].js
+++ src/app/[...id].js
@@ -1,11 +1,40 @@
-"use client"
-import { useRouter } from "next/router"
+import { GetServerSideProps } from 'next';
+import Router from 'next/router';
+import Layout from '../../components/Layout';
+import { useSession } from 'next-auth/react';
+import prisma from '../../lib/prisma';
 
-export default function PageId({params}){
-    const router = useRouter()
-    console.log(router)
-    // const id = params.id
-    // return(
-    //     <h1>{id}</h1>
-    // )
+export const getStaticProps = async ({ params }) => {
+  const id = params.id
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
+  const post = res.json()
+  return {
+    props: post,
+  };
+};
+export const getStaticPath = async ({ params }) => {
+  return{
+    paths:[
+      {params:{id:"1"}},
+      {params:{id:"2"}},
+      {params:{id:"3"}},
+      {params:{id:"4"}},
+      {params:{id:"5"}},
+      {params:{id:"6"}},
+      {params:{id:"7"}},
+      {params:{id:"8"}},
+      {params:{id:"9"}},
+      {params:{id:"10"}},
+    ],
+    fallback: false,
+  }
 }
+
+
+const Post = (props) => {
+  return (
+    <h1>{}</h1>
+  );
+};
+
+export default Post;
